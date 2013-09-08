f29aed9cdbac182409831059c223d434d6d20c0c
Rickard Andersson <rickard.m.andersson@gmail.com>
2013-09-08 15:49:37 +0200

    Removed bold from quack defn face

--- .emacs.el
+++ .emacs.el
@@ -2,6 +2,7 @@
 (let ((default-directory "~/.emacs-load/"))
   (normal-top-level-add-subdirs-to-load-path))
 
+;;; Package management with marmalade.
 (require 'package)
 (add-to-list 'package-archives
             '("marmalade" . "http://marmalade-repo.org/packages/"))
@@ -49,12 +50,14 @@
                  '(progn
                     (color-theme-initialize)
                     (color-theme-hober)))
+
 (custom-set-variables
   ;; custom-set-variables was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(display-battery-mode t)
+ '(haskell-mode-hook (quote (turn-on-haskell-indentation)))
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
  '(quack-default-program "racket")
@@ -70,4 +73,4 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "#202020" :foreground "#c0c0c0" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Ubuntu Mono"))))
- '(quack-pltish-defn-face ((((class color) (background dark)) (:foreground "#2020ff" :weight bold)))))
+ '(quack-pltish-defn-face ((((class color) (background dark)) (:foreground "#5060ff")))))
